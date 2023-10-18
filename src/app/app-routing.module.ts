9b52dcdce4352f80b6f8a02c62a53cd433738440
yhernandez <yhernandez@efisys.com.mx>
2023-10-18 13:24:47 -0600

    exm

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,17 +5,21 @@ import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AuthGuard } from './shared/guards/auth.guard';
 import { MatSidenav } from '@angular/material/sidenav';
+import { SidenavComponent } from './sidenav/sidenav.component';
+
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'sign-up', component: SignUpComponent },
   { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'sidenav', component:  SidenavComponent, canActivate: [AuthGuard] },
   {path:'sidenav',component:MatSidenav, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes),],
+
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
